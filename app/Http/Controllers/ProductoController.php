0b74b682e03d39f8b6e6b37ecfd56584eb3d15df
Your Name <you@example.com>
2022-06-23 10:49:43 -0500

    evidencia cargada

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Producto;
 use App\Models\Marca;
 use App\Models\Categoria;
+//dependencia para el validador
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
@@ -44,22 +46,59 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-       $archivo = $request->imagen;
-       $nombre_archivo = $archivo->getClientOriginalName();
-       
-       $ruta = public_path();
-       $archivo->move("$ruta/img", $nombre_archivo);
-       $producto = new Producto;
-       $producto->nombre = $request->nombre;
-       $producto->descripcion = $request->desc;
-       $producto->precio = $request->precio;
-       $producto->imagen = $nombre_archivo;
-       $producto->marca_id = $request->marca;
-       $producto->categoria_id = $request->categoria;
-       $producto->save();
-       echo "Producto Registrado";
 
+        //1. Validacion
 
+        $reglas=[
+            "nombre" => 'required|alpha|unique:productos,nombre',
+            "desc" => 'required|min:10|max:20',
+            "precio" => 'required|numeric',
+            "imagen" => 'required|image',
+            "categoria" => 'required',
+            "marca" => 'required'
+
+        ];
+
+        $mensajes=[
+            "required" => "Este Campo es Obligatorio",
+            "alpha" => "Este campo solo debe contener Letras",
+            "numeric" => "Este campo es Númerico",
+            "image" => "Carga un archivo tipo imagen",
+            "min" => "Minimo :min valor",
+            "max" => "Maximo :max valor"
+        ];
+
+        //2. crear objeto validador
+        $v = Validator::make($request->all(), $reglas, $mensajes);
+
+        //3. validar
+        //Metodo fails retorna un true si la validacio falla
+        //y false: si los sojn validos
+
+        if($v->fails()){
+            //Validacion incorrecta
+            //Mostrar la vista new 
+            //Llevando los errores
+            return redirect('productos/create')
+                ->withErrors($v);
+        }else{
+            //Validacion correcta
+          $archivo = $request->imagen;
+          $nombre_archivo = $archivo->getClientOriginalName();
+
+          $ruta = public_path();
+          $archivo->move("$ruta/img", $nombre_archivo);
+          $producto = new Producto;
+          $producto->nombre = $request->nombre;
+          $producto->desc = $request->desc;
+          $producto->precio = $request->precio;
+          $producto->imagen = $nombre_archivo;
+          $producto->marca_id = $request->marca;
+          $producto->categoria_id = $request->categoria;
+          $producto->save();
+          return redirect('productos/create')
+          ->with("mensajito", "producto registrado");
+        }
 
     }
 
--- resources/views/productos/new.blade.php
+++ resources/views/productos/new.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.menu')
 
 @section('contenido')
+@if(session('mensajito'))
+<div class="row">
+    <p file-path-wrapper>{{ session('mensajito') }}</p>
+</div>
+@endif
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 <div class="row">
@@ -13,50 +18,58 @@
             <div class="col s8 input-field">
                 <input  type="text" id="nombre" name="nombre" placeholder="Nombre Producto" />
                 <label for="nombre">Nombre Producto</label>
+                <strong>{{ $errors->first('nombre') }}</strong>
             </div>
         </div>
         <div class="row">
             <div class="col s8 input-field">
                 <textarea name="desc" id="desc" class="materialize-textarea" placeholder="Descripcion Producto"></textarea>
                 <label for="desc">Descripción</label>
+                <strong>{{ $errors->first('desc') }}</strong>
             </div>
         </div>
         <div class="row">
             <div class="col s8 input-field">
                 <input type="text" id="precio" name="precio" placeholder="Precio Producto">
                 <label for="precio">Precio Producto</label>
+                <strong>{{ $errors->first('precio') }}</strong>
             </div>
         </div>
         <div class="row">
             <div class="col s8 file-field input-field">
                 <div class="btn"> 
                 <span>Imagen Del Producto</span>
-                <input type="file" name="imagen" />
+                <input type="file" name="imagen"/>
              </div>
             <div class="file-path-wrapper">
                 <input type="text" class="file-path">
             </div>
             </div>
+            <strong>{{ $errors->first('imagen') }}</strong>
         </div>
 
         <div class="row">
             <div class="col s8 input-field">
                 <select name="marca" id="marca">
+                <option value="">Seleccione Marca</option>
                     @foreach($marcas as $marca)
                     <option value="{{$marca->id}}">{{ $marca->nombre }}</option>
                     @endforeach
                 </select>
                 <label>Selecione Marca</label>
+                <strong>{{ $errors->first('marca') }}</strong>
             </div>
         </div>
         <div class="row">
         <div class="col s8 input-field">
                 <select name="categoria" id="categoria">
+                    <option value="">Seleccione Categoria</option>
                      @foreach($categorias as $categoria)
                     <option value="{{$categoria->id}}" >{{ $categoria->nombre }}</option>
                     @endforeach
                 </select>
                 <label>Selecione Categoria</label>
+                <strong>{{ $errors->first('categoria') }}</strong>
                 </div>
         </div>
         <div class="row">
